ab4b224e2878c6c71fc65336c909db30c2834d7d
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2025-01-09 23:41:32 +0330

    feat: add spring cloud gateway

---
+++ api.gateway/src/main/java/com/booking/microservices/java/spring/boot/api/gateway/ApiGatewayConfiguration.java
@@ -0,0 +1,9 @@
+package com.booking.microservices.java.spring.boot.api.gateway;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+
+}
--- flight/src/main/java/com/booking/microservices/java/spring/boot/flight/flights/features/create/flight/CreateFlightController.java
+++ flight/src/main/java/com/booking/microservices/java/spring/boot/flight/flights/features/create/flight/CreateFlightController.java
@@ -4,11 +4,11 @@ import com.booking.microservices.java.spring.boot.flight.flights.dtos.FlightDto;
 import com.booking.microservices.java.spring.boot.flight.flights.features.create.Mappings;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.CompletableFuture;
 
 @RestController
@@ -22,6 +22,7 @@ public class CreateFlightController {
   }
 
   @PostMapping()
+  @PreAuthorize("hasAuthority('ADMIN')")
   public CompletableFuture<ResponseEntity<FlightDto>> createFlight(@RequestBody CreateFlightRequestDto createFlightRequestDto) {
     CreateFlightCommand command = Mappings.toCreateFlightCommand(createFlightRequestDto);
     return commandGateway.send(command);
