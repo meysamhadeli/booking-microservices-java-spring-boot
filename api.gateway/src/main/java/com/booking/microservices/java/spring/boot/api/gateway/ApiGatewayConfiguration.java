898df9fa5196d8ad8bf2332e4edf29faeae7ab16
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2025-01-10 00:56:06 +0330

    refactor: refactor spring cloud gateway

--- api.gateway/src/main/java/com/booking/microservices/java/spring/boot/api/gateway/ApiGatewayConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.booking.microservices.java.spring.boot.api.gateway;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApiGatewayConfiguration {
-
-
-}
