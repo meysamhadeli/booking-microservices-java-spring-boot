79c94ba1dd7b1cfd2897ba1bd850bd71b04124d7
Meysam Hadeli <35596795+meysamhadeli@users.noreply.github.com>
2025-01-19 01:53:46 +0330

    refactor: refactor web client configuration

--- buildingblocks/src/main/java/buildingblocks/web/WebClientConfiguration.java
+++ buildingblocks/src/main/java/buildingblocks/web/WebClientConfiguration.java
@@ -1,13 +1,13 @@
-package io.bookingmicroservices.flight.flights;
+package buildingblocks.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfiguration {
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+public class WebClientConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
